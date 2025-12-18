c83c9ec53d5d2247cf115553b4ae1cd8d9d8bb25
Pouya <pouyamaleki85@gmail.com>
2025-12-18 22:23:27 +0330

    AMS.h

---
+++ AMS.h
@@ -0,0 +1,72 @@
+#ifndef RESOURCEMANAGER_H
+#define RESOURCEMANAGER_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void bubbleSort(vector<TimeInterval> &arr);
+
+class ResourceManager
+{
+private:
+    vector<Resource> resources;
+
+public:
+    static int totalResources;
+    void addResource(const Resource &r);
+    Resource *findById(int id);
+    void printReport() const;
+};
+
+class Time
+{
+private:
+    int hour, minute;
+
+public:
+    Time(int h = 0, int m = 0);
+    void normalize();
+    bool operator<(const Time &) const;
+    bool operator==(const Time &) const;
+    int operator-(const Time &) const;
+    string toString() const;
+};
+
+class TimeInterval
+{
+private:
+    Time start, end;
+
+public:
+    TimeInterval(const Time &s = Time(), const Time &e = Time());
+    bool overlaps(const TimeInterval &) const;
+    bool contiguous(const TimeInterval &) const;
+    int duration() const;
+    bool merge(const TimeInterval &, TimeInterval &) const;
+    TimeInterval operator+(const TimeInterval &) const;
+    bool operator<(const TimeInterval &) const;
+    bool operator==(const TimeInterval &) const;
+    string toString() const;
+};
+
+class Resource
+{
+private:
+    int id;
+    string name;
+    vector<TimeInterval> intervals;
+
+public:
+    Resource(int _id = 0, string _name = "");
+    int getId() const;
+    const string &getName() const;
+    bool hasConflict(const TimeInterval &) const;
+    bool addInterval(const TimeInterval &);
+    void normalize();
+    void printSchedule() const;
+};
+
+#endif
